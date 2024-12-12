589b3e684b20e5909ebbd3c43a179223b44f8661
Jullyana <jullyana.brasileiro@hotmail.com>
2024-12-12 12:18:33 -0800

    DIY 08

--- DIY 08/diy08.js
+++ DIY 08/diy08.js
@@ -4,7 +4,8 @@ const fs = require('fs/promises');
 
 async function writeNameAsync() {
     try {
-        
+        await fs.writeFile('test.txt', 'Jullyana Brasileiro');
+        console.log('File written successfully with Async function');
     } catch (error) {
         console.error('An error occurred while writing the file.', err);
     }
