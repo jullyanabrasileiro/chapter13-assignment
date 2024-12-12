4ecb0912529f20737104fa5a72e8b79aa1409e2e
Jullyana <jullyana.brasileiro@hotmail.com>
2024-12-12 11:20:26 -0800

    DIY 06

--- DIY 06/diy06.js
+++ DIY 06/diy06.js
@@ -1,10 +1,10 @@
 // Write a program that reads asynchronous from a file called test.txt and prints the text to the console
 
-const fsp = require('fs/promises');
+const fs = require('fs/promises');
 
 async function readFileAsync() {
     try {
-        const data = await fsp.readFile('syncAndAsync.txt', 'utf8');
+        const data = await fs.readFile('syncAndAsync.txt', 'utf8');
         console.log(data);
 
     } catch (err) {
